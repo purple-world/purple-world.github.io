213463e1f5b040aec4eee251ef312f5c4c498d88
cscawley <cscawley@mac.com>
2022-12-05 18:47:19 -0600

    update site description

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://purpleworld.gg/precache-manifest.498a39bd8ff370f5846c642455c004fe.js"
+  "https://purpleworld.gg/precache-manifest.0008dafd728eb08de6a975b5ab78fe2f.js"
 );
 
 self.addEventListener('message', (event) => {

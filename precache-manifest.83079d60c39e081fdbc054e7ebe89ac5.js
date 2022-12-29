7c7fd8784e07d4a7934f2e545c4bbd01cf3d8a92
cscawley <cscawley@mac.com>
2022-12-29 15:45:38 -0600

    update Rules copy

---
+++ precache-manifest.83079d60c39e081fdbc054e7ebe89ac5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1769ebded687efd48565583d6ab10026",
+    "url": "https://purpleworld.gg/index.html"
+  },
+  {
+    "revision": "70dc51be1a4346477f72",
+    "url": "https://purpleworld.gg/static/css/main.f046fba5.chunk.css"
+  },
+  {
+    "revision": "01b85ca2faa721430fe6",
+    "url": "https://purpleworld.gg/static/js/2.a1a16796.chunk.js"
+  },
+  {
+    "revision": "d5b473ff893d6b5fac7d0b5081a5072a",
+    "url": "https://purpleworld.gg/static/js/2.a1a16796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70dc51be1a4346477f72",
+    "url": "https://purpleworld.gg/static/js/main.60813290.chunk.js"
+  },
+  {
+    "revision": "488338b5ad192c511d63",
+    "url": "https://purpleworld.gg/static/js/runtime-main.a173ad01.js"
+  },
+  {
+    "revision": "79c5962cbd39556e446ab2f0c51cec99",
+    "url": "https://purpleworld.gg/static/media/Discord-Emblem.79c5962c.png"
+  },
+  {
+    "revision": "7a1e901800df419cfeeed3b80b30260f",
+    "url": "https://purpleworld.gg/static/media/TT2020StyleBRegular.7a1e9018.ttf"
+  },
+  {
+    "revision": "b20f6c47d95c00ce0fda959192130ea3",
+    "url": "https://purpleworld.gg/static/media/bureaucrat.b20f6c47.png"
+  },
+  {
+    "revision": "5794b8d5f2bcbf960124bd32e552cbe4",
+    "url": "https://purpleworld.gg/static/media/coastguard.5794b8d5.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "https://purpleworld.gg/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0376e513b2b3c1f0452bffd79da84329",
+    "url": "https://purpleworld.gg/static/media/farmer.0376e513.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "https://purpleworld.gg/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "https://purpleworld.gg/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "e66818efb2da528205c8846ad25834f7",
+    "url": "https://purpleworld.gg/static/media/logo.e66818ef.svg"
+  },
+  {
+    "revision": "b52cb8a548ccf4e10f2360b19cbcdf03",
+    "url": "https://purpleworld.gg/static/media/map.b52cb8a5.png"
+  },
+  {
+    "revision": "4f69865a83e0759484d4438a85295979",
+    "url": "https://purpleworld.gg/static/media/map2.4f69865a.png"
+  },
+  {
+    "revision": "782e05e9791e40d06a6c2d2b2653d826",
+    "url": "https://purpleworld.gg/static/media/phase-1.782e05e9.png"
+  },
+  {
+    "revision": "75edd59c9ed881c4d88468f78d2d2bb9",
+    "url": "https://purpleworld.gg/static/media/phase-2.75edd59c.png"
+  },
+  {
+    "revision": "2e8ce94230c6c70b91f5370a34f0cf7e",
+    "url": "https://purpleworld.gg/static/media/phase-3.2e8ce942.png"
+  }
+]);
